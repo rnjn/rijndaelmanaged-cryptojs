8cdcab1be63938cbd1e6563002eccc044af0fcfe
Ranjan Sakalley <rsakalle@thoughtworks.com>
2013-06-05 11:23:08 +0530

    decryption works now

--- rijndael.js
+++ rijndael.js
@@ -6,10 +6,17 @@ var rijndaelEncrypt = function(){
   document.getElementById('encrypted').innerText = encryptedText;
 }
 
+var rijndaelDecrypt = function(){
+  document.getElementById('decrypted').innerText = "";
+  var encryptedText = document.getElementById('encrypted').textContent;
+  var key = document.getElementById('key').value;
+  var decryptedText = decrypt(encryptedText, key);
+  document.getElementById('decrypted').innerText = decryptedText;
+}
+
 var encrypt = function(plainText, key){
   var C = CryptoJS;
   plainText = C.enc.Utf8.parse(plainText);
-  console.log(plainText);
   key = C.enc.Utf8.parse(key);
   var aes = C.algo.AES.createEncryptor(key, {
     mode: C.mode.CBC,
@@ -18,4 +25,17 @@ var encrypt = function(plainText, key){
   });
   var encrypted = aes.finalize(plainText);
   return C.enc.Base64.stringify(encrypted);
+}
+
+var decrypt = function(encryptedText, key){
+  var C = CryptoJS;
+  encryptedText = C.enc.Base64.parse(encryptedText);
+  key = C.enc.Utf8.parse(key);
+  var aes = C.algo.AES.createDecryptor(key, {
+    mode: C.mode.CBC,
+    padding: C.pad.Pkcs7,
+    iv: key
+  });
+  var decrypted = aes.finalize(encryptedText);
+  return C.enc.Utf8.stringify(decrypted);
 }
